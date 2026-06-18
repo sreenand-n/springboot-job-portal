9e8c1864591e8d1095c099878958612d81c132e0
sreenand-n <work.sreenand@gmail.com>
2026-06-18 20:41:32 +0530

    Spring Boot Job Portal Application

---
+++ JobApp/src/main/java/com/example/JobApp/JobController.java
@@ -0,0 +1,39 @@
+package com.example.JobApp;
+import com.example.JobApp.model.JobPost;
+import com.example.JobApp.service.JobService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import java.util.List;
+
+@Controller
+public class JobController {
+
+    @Autowired
+    private JobService service;
+
+    @GetMapping({"/", "home"})
+    public String home(){
+        return "home.jsp";
+    }
+
+    @GetMapping("addjob")
+    public String addJob(){
+        return "addjob.jsp";
+    }
+
+    @PostMapping("handleForm")  // Doing the mapping for post method
+    public String handleForm(JobPost jobPost){
+        service.addJob(jobPost);
+        return "success.jsp";
+    }
+
+    @GetMapping("viewalljobs")
+    public String viewJobs(Model model){
+        List<JobPost> jobs = service.getAllJobs();
+        model.addAttribute("jobPosts", jobs);
+        return "viewalljobs.jsp";
+    }
+}
